7a49c83117e22a062f40d8cd7515a35fed6272e2
Maximiliano Arvizu <153956469+Marvizu2304@users.noreply.github.com>
2025-04-24 10:59:24 -0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+column_aliases = {
+    "Academic Level": ["academic level", "level", "education level"],
+    "Sex": ["sex", "gender"],
+    "Disability": ["disability", "disability status"],
+    "Major": ["major", "program", "field of study"],
+    "Destination Country": ["destination", "destination country", "host country"],
+    "Program Start Date": ["start date", "program start date"],
+    "Program End Date": ["end date", "program end date"]
+}
